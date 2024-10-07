f2c8a910f0f943260b148ae755012fb9a2dc92bc
MajoPino <majo12pino@gmail.com>
2024-10-07 09:23:35 -0500

    feat: Creating the tables of each model in the database

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,8 +1,15 @@
 using Microsoft.EntityFrameworkCore;
+using PruebaNET_MariajosePino.Models;
 
 namespace PruebaNET_MariajosePino.Data;
 
 public class AppDbContext : DbContext
 {
-    
+    public DbSet<Booking> Bookings { get; set; }
+    public DbSet<Guest> Guests { get; set; }
+    public DbSet<Employee> Employees { get; set; }
+    public DbSet<Room> Rooms { get; set; }
+    public DbSet<RoomType> RoomTypes { get; set; }
+
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
 }
